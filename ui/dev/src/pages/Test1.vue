128599d89133cff17bf5ab8fabeb342358be2ca7
nymphaea-v1 <roselin.6@yandex.ru>
2023-06-20 21:26:02 +0500

    Feat(dev): add search toggle and format test page

--- ui/dev/src/pages/Test1.vue
+++ ui/dev/src/pages/Test1.vue
@@ -1,42 +1,78 @@
 <template>
-  <q-page padding class="column items-center">
+  <q-page
+    padding
+    class="column items-center"
+  >
     <h5>Test q-tel-input:</h5>
-    <q-tel-input  v-model="phone" v-bind="inputProps" style="width: 300px;" />
-    <div class="row q-gutter" style="width: 500px;">
-      <p class="text-body1 q-ma-none col-2 text-right">Value:</p>
-      <p class="col-10 q-pl-md">{{ phone }}</p>
 
-      <p class="text-body1 q-ma-none col-2 text-right">Options:</p>
+    <q-tel-input
+      v-model="phone"
+      v-bind="inputProps"
+      style="width: 300px;"
+    />
+
+    <div
+      class="row q-gutter"
+      style="width: 500px;"
+    >
+      <p class="text-body1 q-ma-none col-2 text-right">
+        Value:
+      </p>
+      <p class="col-10 q-pl-md">
+        {{ phone }}
+      </p>
+
+      <p class="text-body1 q-ma-none col-2 text-right">
+        Options:
+      </p>
       <div class="col-10 q-pl-md">
-        <q-checkbox v-model="inputProps.outlined" label="outlined" />
-        <q-checkbox v-model="inputProps.dense" label="dense" />
-        <q-checkbox v-model="inputProps.filled" label="filled" />
-        <q-checkbox v-model="inputProps.borderless" label="borderless" />
-        <q-checkbox v-model="inputProps.rounded" label="rounded" />
+        <q-checkbox
+          v-for="(option, index) in TOGGLE_OPTIONS"
+          :key="index"
+          v-model="inputProps[option]"
+          :label="option"
+        />
       </div>
 
-      <p class="text-body1 q-ma-none col-2 text-right">Label:</p>
-      <q-input class="col-10 q-pl-md" :model-value="inputProps.label" :hint="LABEL_HINT" dense @update:model-value="updateLabel"></q-input>
+      <p class="text-body1 q-ma-none col-2 text-right">
+        Label:
+      </p>
+      <q-input
+        class="col-10 q-pl-md"
+        :model-value="inputProps.label"
+        :hint="LABEL_HINT"
+        dense
+        @update:model-value="updateLabel"
+      />
     </div>
   </q-page>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 
 const phone = ref('+12345678900')
+
+const TOGGLE_OPTIONS = [
+  'outlined',
+  'dense',
+  'filled',
+  'borderless',
+  'rounded',
+  'search'
+]
+
 const inputProps = ref({
-  outlined: false,
-  dense: false,
-  filled: false,
-  borderless: false,
-  rounded: false,
   label: undefined
 })
+
+onMounted(() => {
+  TOGGLE_OPTIONS.forEach((option) => inputProps.value[option] = false)
+})
+
+// const LABEL_HINT = 'Label shifts national number. IMHO it looks pretty bad and fixing it in my TODO list'
 
 const updateLabel = (value) => {
   inputProps.value.label = value === '' ? undefined : value
 }
-
-const LABEL_HINT = 'Label shifts national number. IMHO it looks pretty bad and fixing it in my TODO list'
 </script>
