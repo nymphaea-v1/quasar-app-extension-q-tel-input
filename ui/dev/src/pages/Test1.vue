7d78f71eac907e58cb128a5c03c69d574dd0f123
nymphaea-v1 <roselin.6@yandex.ru>
2023-06-21 11:13:35 +0500

    Feat(dev): add to test page strictness, exposed

--- ui/dev/src/pages/Test1.vue
+++ ui/dev/src/pages/Test1.vue
@@ -6,13 +6,14 @@
     <h5>Test q-tel-input:</h5>
 
     <q-tel-input
+      ref="element"
       v-model="phone"
       v-bind="inputProps"
       style="width: 300px;"
     />
 
     <div
-      class="row q-gutter"
+      class="row q-gutter items-baseline"
       style="width: 500px;"
     >
       <p class="text-body1 q-ma-none col-2 text-right">
@@ -21,6 +22,26 @@
       <p class="col-10 q-pl-md">
         {{ phone }}
       </p>
+
+      <p class="text-body1 q-ma-none col-2 text-right">
+        Exposed:
+      </p>
+      <p class="col-10 q-pl-md">
+        {{ element }}
+      </p>
+
+      <p class="text-body1 q-ma-none col-2 text-right">
+        Strictness:
+      </p>
+      <div class="col-10 q-pl-md">
+        <q-radio
+          v-for="(value, index) in STRICTNESS"
+          :key="index"
+          v-model="inputProps.strictness"
+          :val="value"
+          :label="value"
+        />
+      </div>
 
       <p class="text-body1 q-ma-none col-2 text-right">
         Options:
@@ -52,6 +73,7 @@
 import { ref, onMounted } from 'vue'
 
 const phone = ref('+12345678900')
+const element = ref()
 
 const TOGGLE_OPTIONS = [
   'outlined',
@@ -62,12 +84,19 @@ const TOGGLE_OPTIONS = [
   'search'
 ]
 
+const STRICTNESS = [
+  'NONE',
+  'LENGTH',
+  'FULL'
+]
+
 const inputProps = ref({
-  label: undefined
+  label: undefined,
+  strictness: 'FULL'
 })
 
 onMounted(() => {
-  TOGGLE_OPTIONS.forEach((option) => inputProps.value[option] = false)
+  TOGGLE_OPTIONS.forEach((option) => (inputProps.value[option] = false))
 })
 
 const LABEL_HINT = 'Label shifts national number. IMHO it looks pretty bad and fixing it in my TODO list'
